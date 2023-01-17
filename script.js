acd0e28b4a2dbcc0b2291f8950b482a51efbaa0a
dineshthapa2000@gmail.com <dineshthapa2000@gmail.com>
2023-01-17 17:36:14 +0000

    password generator system working after implementation of javascript

---
+++ script.js
@@ -0,0 +1,77 @@
+//dom element
+const generateEl=document.getElementById('generate');
+const lengthEl=document.getElementById('myRange');
+const upperEl=document.getElementById('upperCase')
+const lowerEl=document.getElementById('lowerCase');
+const numberEl=document.getElementById('num');
+const symbolEl=document.getElementById('specialChar');
+const resultEl=document.getElementById('password');
+
+
+const randomFunc= {
+    lower:randomLowerCase,
+    upper: randomUpperCase,
+    number: randomNumber,
+    symbols:randomSymbol
+};
+//generate event listener
+generateEl.addEventListener('click',() =>{
+    const length=lengthEl.value;
+    const hasLower=lowerEl.checked;
+    const hasUpper=upperEl.checked;
+    const hassymbol=symbolEl.checked;
+    const hasnumber=numberEl.checked;
+    
+
+    resultEl.innerText=generatePassword(hasLower,hasUpper,hasnumber,hassymbol,length);
+});
+
+//generate password function
+function generatePassword(lower,upper,number,symbols,length){
+
+    //initialize password variable
+    //filter out unchecked type
+    //loop through length
+    //return result to password variable
+    let generatedPassword='';
+    const typesCount=lower+upper+number+symbols;
+    console.log("types Count: " +typesCount);
+    const typeArr=[{lower},{upper},{symbols},{number}].filter
+    (
+        item=>Object.values(item)[0]
+    );
+    
+    if(typesCount===0){
+        return "";
+    }
+    for(let i=0; i<length; i+=typesCount){
+        typeArr.forEach(type => {
+            const funcName=Object.keys(type)[0];
+
+            generatedPassword+=randomFunc[funcName]();
+            
+        });
+    }
+    const finalPassword=generatedPassword.slice(0,length);
+    return finalPassword;
+
+}
+// define function to generate random number, symbol, uppercase,lower case
+function randomLowerCase(){
+    return String.fromCharCode(Math.floor(Math.random()*26)+97);
+}
+
+function randomUpperCase(){
+    return String.fromCharCode(Math.floor(Math.random()*26)+65);
+}
+
+function randomNumber(){
+    return String.fromCharCode(Math.floor(Math.random()*10)+48);
+}
+
+function randomSymbol(){
+    const symbols=  '@%+\\/!#$^?:)(}{][~-_.'
+    return symbols[Math.floor(Math.random()*symbols.length)];
+}
+
+console.log(randomSymbol());
